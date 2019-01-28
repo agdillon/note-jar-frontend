25b60da73c1b2544948f02e9d6592c35023dccf2
Amanda Dillon <agdillon@gmail.com>
2019-01-27 19:09:29 -0700

    fetch data from backend and render notes

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import NoteList from './components/NoteList'
 
 export default class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      loggedInUser: 1,
+      notes: []
+    }
+  }
+
+  async componentDidMount() {
+    const response = await fetch(`http://note-jar.herokuapp.com/users/${this.state.loggedInUser}/notes`)
+    const notes = await response.json()
+    this.setState({ notes })
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Hello World</Text>
+        <NoteList notes={this.state.notes} />
       </View>
     )
   }
---
+++ components/NoteList.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { StyleSheet, Text, View, FlatList } from 'react-native'
+
+function NoteList({ notes }) {
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={notes}
+        keyExtractor={note => note.note_id.toString()}
+        renderItem={({ item }) => <Text style={styles.item}>{item.content}</Text> }
+      />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 22
+  },
+  item: {
+    padding: 10,
+    fontSize: 18
+  }
+})
+
+export default NoteList
